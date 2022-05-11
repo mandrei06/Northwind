f4b9872563f671f0c32c4ef5efe12a20e872f521
mandrei06 <marincasandrei06@gmail.com>
2022-05-11 15:35:42 +0100

    GET(for all the classes)+POST(only for 2 classes)

--- Northwind/src/main/java/com/sparta/northwind/controller/NorthwindController.java
+++ Northwind/src/main/java/com/sparta/northwind/controller/NorthwindController.java
@@ -1,13 +1,16 @@
 package com.sparta.northwind.controller;
 
 import com.sparta.northwind.entities.Customer;
+import com.sparta.northwind.entities.Employee;
+import com.sparta.northwind.entities.Order;
+import com.sparta.northwind.entities.Shipper;
 import com.sparta.northwind.repositories.CustomerRepository;
 import com.sparta.northwind.repositories.EmployeeRepository;
 import com.sparta.northwind.repositories.OrderRepository;
 import com.sparta.northwind.repositories.ShipperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,8 +26,68 @@ public class NorthwindController {
     @Autowired
     private ShipperRepository shipperRepository;
 
+
+    @PostMapping(value = "addCustomer")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void addCustomer(@RequestBody Customer params) {
+        Customer customer=new Customer();
+        customer.setId(params.getId());
+        customer.setCompanyName(params.getCompanyName());
+        customer.setContactName(params.getContactName());
+        customer.setContactTitle(params.getContactTitle());
+        customer.setAddress(params.getAddress());
+        customer.setCity(params.getCity());
+        customer.setRegion(params.getRegion());
+        customer.setPostalCode(params.getPostalCode());
+        customer.setCountry(params.getCountry());
+        customer.setPhone(params.getPhone());
+        customer.setFax(params.getFax());
+
+        customerRepository.save(customer);
+    }
+    @PostMapping(value = "addEmployee")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void addEmployee(@RequestBody Employee params){
+        Employee employee=new Employee();
+        employee.setId(params.getId());
+        employee.setLastName(params.getLastName());
+        employee.setFirstName(params.getFirstName());
+        employee.setTitle(params.getTitle());
+        employee.setTitleOfCourtesy(params.getTitleOfCourtesy());
+        employee.setBirthDate(params.getBirthDate());
+        employee.setHireDate(params.getHireDate());
+        employee.setAddress(params.getAddress());
+        employee.setCity(params.getCity());
+        employee.setRegion(params.getRegion());
+        employee.setPostalCode(params.getPostalCode());
+        employee.setCountry(params.getCountry());
+        employee.setHomePhone(params.getHomePhone());
+        employee.setExtension(params.getExtension());
+        employee.setPhoto(params.getPhoto());
+        employee.setReportsTo(params.getReportsTo());
+        employee.setPhotoPath(params.getPhotoPath());
+        employee.setSalary(params.getSalary());
+
+        employeeRepository.save(employee);
+    }
+
+
+    //READ
+    //GET for all our 4 classes
     @GetMapping("/allCustomers")
     public List<Customer> getAllCustomers(){
         return customerRepository.findAll();
+    }
+    @GetMapping("/allEmployees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+    @GetMapping("/allOrders")
+    public List<Order> getAllOrders(){
+        return orderRepository.findAll();
+    }
+    @GetMapping("/allShippers")
+    public List<Shipper> getAllShippers(){
+        return shipperRepository.findAll();
     }
 }
