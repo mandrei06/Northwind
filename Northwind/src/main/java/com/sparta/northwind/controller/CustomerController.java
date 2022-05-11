63ac0d889cb0bbe1ad45fea7e55c1cc926931087
mandrei06 <marincasandrei06@gmail.com>
2022-05-11 16:46:08 +0100

    GET+POST+PUT+DELETE for Customers Table

--- Northwind/src/main/java/com/sparta/northwind/controller/CustomerController.java
+++ Northwind/src/main/java/com/sparta/northwind/controller/CustomerController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class NorthwindController {
+public class CustomerController {
 
     @Autowired
     private CustomerRepository customerRepository;
